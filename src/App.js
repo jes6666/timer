37dc101db8f8c43e210e5aab5126c40332e8c7f2
Evgeny <evgeny.klochko@effective-soft.com>
2022-05-24 06:59:18 +0400

    Create countdown

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
-import CountdownTimer from 'react-awesome-countdowntimer';
 import moment from 'moment';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      const today = moment();
+      const threshold = moment('06-15-2022 7:00 PM');
+
+      setDays(format(threshold.diff(today, 'days')));
+      setHours(format(threshold.diff(today, 'hours') % 24));
+      setMinutes(format(threshold.diff(today, 'minutes') % 60));
+      setSeconds(format(threshold.diff(today, 'seconds') % 60));
+      console.log();
+    }, 1000);
+  }, [seconds]);
+
+  const format = (value) => {
+    if (value.toString().length < 2) {
+      return `0${value}`
+    }
+
+    return value;
+  }
+
   return (
     <div className="App">
       <header>
@@ -18,7 +43,24 @@ function App() {
       </header>
       <main>
         <div className="wrapper timer">
-          <CountdownTimer endDate={moment('06-15-2022 7:00 PM')}/>
+          <div>
+            <div className="section">
+              <div className="time">{days}</div>
+              <div className="label">Days</div>
+            </div>
+            <div className="section">
+              <div className="time">{hours}</div>
+              <div className="label">Hours</div>
+            </div>
+            <div className="section">
+              <div className="time">{minutes}</div>
+              <div className="label">Minutes</div>
+            </div>
+            <div className="section">
+              <div className="time">{seconds}</div>
+              <div className="label">Seconds</div>
+            </div>
+          </div>
         </div>
       </main>
     </div>
